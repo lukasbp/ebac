b44270918ddafa2e8bd88cdda4cb94688a615b75
lucas pereira <darksleepo@gmail.com>
2023-08-18 00:30:02 -0300

    listas e variaveis

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
- 
+
+import { faker } from '@faker-js/faker';
+
 describe('Funcionalidade pré cadastro', () => {
 
     beforeEach(() => {
@@ -7,12 +9,16 @@ describe('Funcionalidade pré cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', ()=>{
-        cy.get('#reg_email').type(faker.internet.email())
+        let nomeFaker = faker.name.firstName();
+        let sobrenomeFaker = faker.name.lastName();
+        let emailFaker = faker.internet.email();
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type('jair')
-        cy.get('#account_last_name').type('lulonaro')
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta')
