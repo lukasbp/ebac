66437bdb02b5402ad88951aed17c6f78f008250d
lucas pereira <darksleepo@gmail.com>
2023-08-21 00:23:58 -0300

    fixtures e massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -33,6 +34,24 @@ context('Funcionalidade Login', () => {
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-error').should('contain', 'a senha fornecida')
+    })
+
+    it('Deve fazer login com sucesso - Usando arquivos de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it('Deve fazer login com sucesso - Usando fixture', () =>{
+        cy.fixture('perfil').then(dados =>{
+            cy.get('#username').type(perfil.usuario)
+            cy.get('#password').type(perfil.senha, {log:false})
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
     })
 
 
--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -5,7 +5,7 @@ import { faker } from '@faker-js/faker';
 describe('Funcionalidade pré cadastro', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     it('Deve completar o pré cadastro com sucesso', ()=>{
