3f223a5962e645e70b4bd6a350d92f0db4c9a5a2
lucas pereira <darksleepo@gmail.com>
2023-08-27 15:46:02 -0300

    page objects e dados em lista

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,16 +1,36 @@
 /// <reference types="cypress"/>
 
+import enderecoPage from "../support/page-objects/endereco-page";
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
-    
+
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
     });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () =>{
-        
-        //cadastro de endereço
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('bundas', 'sujas', 'cds', 'brasil', 'jose atizano', '331', 'cornelio procopio', 'parana', '86300000', '43998082841', 'bundassujas@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    })
+
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].rua,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].celular,
+            dadosEndereco[1].email
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     })
 });
